15001b4666c1c4d810d5a32fd5a5abfd6abc41e9
n0body <n0bodyyt.buisness@gmail.com>
2015-12-24 12:44:14 +0100

    First Commit

--- app/src/main/java/com/example/joshua/helloworld/MainActivity.java
+++ app/src/main/java/com/example/joshua/helloworld/MainActivity.java
@@ -11,6 +11,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    String test = new String("Test");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
